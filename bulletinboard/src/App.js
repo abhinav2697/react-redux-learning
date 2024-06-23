6b5060b5aeefddfadb1d7119de4d40050f4d69c6
abhinav2697 <abhinav2697@gmail.com>
2024-06-23 23:51:56 +0530

    new files added

---
+++ bulletinboard/src/App.js
@@ -0,0 +1,12 @@
+import PostList from './features/posts/PostList';
+
+
+function App() {
+  return (
+    <div className="App">
+    <PostList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bulletinboard/src/features/posts/AddPostForm.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const AddPostForm = () => {
+    return (
+        <div>Add Post Form</div>
+    )
+}
+export default AddPostForm;
