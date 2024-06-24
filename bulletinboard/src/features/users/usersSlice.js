a3de67cec03ad3f8cd1f0daf125bea6c62333e37
abhinav2697 <abhinav2697@gmail.com>
2024-06-25 01:01:15 +0530

    new features added

--- bulletinboard/src/features/users/usersSlice.js
+++ bulletinboard/src/features/users/usersSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
-    { id: '0', name: 'Raju' },
-    { id: '1', name: 'Laxman' },
-    {id:'2',name:'Bharath'}
+    { id: '0', name: 'William Shakespeare' },
+    { id: '1', name: 'Agatha Christie' },
+    {id:'2',name:'J. K. Rowling'}
 ]
 const usersSlice = createSlice({
     name: 'users',
