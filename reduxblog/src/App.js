41f02ac7e496eda8dbf8f640196980735a04b3b4
abhinav2697 <abhinav2697@gmail.com>
2024-06-27 01:01:01 +0530

    new files and features added

---
+++ reduxblog/src/App.js
@@ -0,0 +1,21 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<PostsList />} />
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage/>}/>
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
