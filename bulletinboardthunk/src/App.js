c72020fc1fe5a0489f32ebc1fe451962d66955bb
abhinav2697 <abhinav2697@gmail.com>
2024-06-26 23:16:28 +0530

    new files and features added

---
+++ bulletinboardthunk/src/App.js
@@ -0,0 +1,14 @@
+import PostList from './features/posts/PostList';
+import AddPostForm from './features/posts/AddPostForm';
+
+function App() {
+  return (
+    <div className="App">
+        <AddPostForm/>
+      <PostList />
+    
+    </div>
+  );
+}
+
+export default App;
