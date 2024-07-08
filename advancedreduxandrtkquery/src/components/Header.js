6c24384a72140e5a1fe96fb3b64b37d4cb88421b
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-07-08 23:45:35 +0530

    Update Header.js

--- advancedreduxandrtkquery/src/components/Header.js
+++ advancedreduxandrtkquery/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <header className="Header">
-            <h1>Redux Blog</h1>
+            <h1>Redux Blog Here</h1>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
